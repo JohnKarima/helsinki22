d534c9d4f888b8a7d3123b1cf212aeeb09de8191
JohnKarima <karimajohn24@gmail.com>
2022-09-16 17:41:01 +0300

    1.3. Implemented the use of objects in the application

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,34 +1,55 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
+// const Header = (props) => {
+//   return <h1>{props.course}</h1>
+// }
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.partName} {props.exerciseNumber}
-    </p>
-  )
-}
-const Content = (props) => {
-  return (
-    <p>
-      <Part partName="Fundamentals of React" exerciseNumber={10} />
-      <Part partName="Using props to pass data" exerciseNumber={7} />
-      <Part partName="State of a component" exerciseNumber={14} />
-    </p>
-  )
-}
+// const Part = (props) => {
+//   return (
+//     <p>
+//       {props.partName} {props.exerciseNumber}
+//     </p>
+//   )
+// }
+// const Content = (props) => {
+//   return (
+//     <p>
+//       <Part partName="Fundamentals of React" exerciseNumber={10} />
+//       <Part partName="Using props to pass data" exerciseNumber={7} />
+//       <Part partName="State of a component" exerciseNumber={14} />
+//     </p>
+//   )
+// }
 
-const Total = (props) => {
-  return <p>Number of exercises {props.totalNumber}</p>
-}
+// const Total = (props) => {
+//   return <p>Number of exercises {props.totalNumber}</p>
+// }
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
+
   return (
     <div>
-      <Header course="Half Stack application development" />
-      <Content />
-      <Total totalNumber={10 + 7 + 14} />
+      <h1>{course}</h1>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part2.exercises}
+      </p>
     </div>
   )
 }

f9b1ab43109830e5f0ad0ceb5f72a5380581ee61
JohnKarima <karimajohn24@gmail.com>
2022-09-17 17:09:32 +0300

    1.5. Change course and parts into single JS object and work from said object

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,40 +1,48 @@
 const Header = (props) => {
-  //   console.log(props)
-  return <h1> {props.course}</h1>
+  console.log(props)
+  return <h1> {props.course.name}</h1>
 }
 
 const Content = (props) => {
   console.log(props)
   return (
     <>
-      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+      <p>
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
+      <Content parts={course.parts} />
     </div>
   )
 }

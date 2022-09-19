27eb3f88456cdbb582699e2451c279fe91cea63b
JohnKarima <karimajohn24@gmail.com>
2022-09-19 16:46:04 +0300

    Review destructuring and work through examples

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,48 +1,21 @@
-const Header = (props) => {
-  console.log(props)
-  return <h1> {props.course.name}</h1>
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  console.log(props)
-  return (
-    <>
-      <p>
-        {props.parts[0].name} {props.parts[0].exercises}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exercises}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exercises}
-      </p>
-    </>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = (onClick, text) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  }
+  const [counter, setCounter] = useState(0)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
     </div>
   )
 }
